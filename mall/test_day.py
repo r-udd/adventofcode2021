29369cc848bec52295a6e51f68bf266e4304d46d
Robert Udd <robert.udd@lm.se>
2021-12-03 15:00:52 +0100

    Mall

---
+++ mall/test_day.py
@@ -0,0 +1,21 @@
+from itertools import *
+
+example_1 = 42
+
+def solve_a (data):
+    return data
+
+def solve_b(data):
+    return data
+
+def test_xxa():
+    assert solve_a(example_1) == 42
+
+def test_xxb():
+    assert solve_b(example_1) == 42
+
+def test_1(dayxx_lines):
+    assert solve_a(dayxx_lines) == 42
+
+def test_b(dayxx_numbers):
+    assert solve_b(dayxx_numbers) == 42
