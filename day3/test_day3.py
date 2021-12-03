ea5bac40b42a93409dc06e09be08d1ec7827ebfe
Robert Udd <robert.udd@lm.se>
2021-12-03 15:01:16 +0100

    Första del dag 3

---
+++ day3/test_day3.py
@@ -0,0 +1,33 @@
+from itertools import *
+
+example = ['00100', '11110', '10110', '10111', '10101', '01111', '00111', '11100', '10000', '11001', '00010', '01010']
+
+def solve_a (numbers):
+    half = len(numbers) / 2
+    ones = [x.count('1') > half for x in zip(*numbers)]
+    gamma = ''
+    epsilon = ''
+    for e in ones:
+        if e:
+            gamma += '1'
+            epsilon += '0'
+        else:
+            gamma += '0'
+            epsilon += '1'
+
+    return int(gamma, 2) * int(epsilon, 2)
+
+def solve_b(numbers):
+    return sum(map(lambda x: x[0] < x[1], zip(numbers, numbers[3:])))
+
+def test_03a():
+    assert solve_a(example) == 198
+
+def test_03b():
+    assert solve_b(example) == 230
+
+def test_a(day03_lines):
+    assert solve_a(day03_lines) == 4147524
+
+def test_b(day03_numbers):
+    assert solve_b(day03_numbers) == 1150
