609da3e00c800e8aea987224decc15a024061c6c
Robert Udd <robert.udd@lm.se>
2021-12-03 15:56:11 +0100

    Dag 3 klar

--- day3/test_day3.py
+++ day3/test_day3.py
@@ -18,7 +18,31 @@ def solve_a (numbers):
     return int(gamma, 2) * int(epsilon, 2)
 
 def solve_b(numbers):
-    return 0
+    index = 0
+    oxygen = numbers
+    while len(oxygen) > 1:
+        half = len(oxygen) / 2
+        summa = sum(map(lambda x: 1 if x[index] == '1' else 0, oxygen))
+        if summa >= half:
+            eq = '1'
+        else:
+            eq = '0'
+        oxygen = list(filter(lambda x: x[index] == eq, oxygen))
+        index += 1
+
+    index = 0
+    co2 = numbers
+    while len(co2) > 1:
+        half = len(co2) / 2
+        summa = sum(map(lambda x: 1 if x[index] == '1' else 0, co2))
+        if summa >= half:
+            eq = '0'
+        else:
+            eq = '1'
+        co2 = list(filter(lambda x: x[index] == eq, co2))
+        index += 1
+
+    return int(oxygen[0], 2) * int(co2[0], 2)
 
 def test_03a():
     assert solve_a(example) == 198
@@ -29,5 +53,5 @@ def test_03b():
 def test_a(day03_lines):
     assert solve_a(day03_lines) == 4147524
 
-def test_b(day03_numbers):
-    assert solve_b(day03_numbers) == 1150
+def test_b(day03_lines):
+    assert solve_b(day03_lines) == 3570354
